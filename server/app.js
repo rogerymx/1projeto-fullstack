ef463d36bfb0806041c72903f3ed5c944b9c43af
Luís Felipe Vidotti <127566200+rogerymx@users.noreply.github.com>
2025-02-11 16:18:45 -0300

    configuracao do server e definicao dos modelos

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//config
+    //mongoose
+    mongoose.connect("mongodb+srv://rogmot:vIFRuQlHP7nUAadG@cluster0.zrbpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+        console.log("Conectado ao banco de dados com sucesso!!!");
+    }).catch((erro) => {
+        console.log("Erro ao se conectar ao banco de dados, erro: "+erro);
+    })
+
+    //body-parser
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+//rotas
+app.get("/", (req,res) => {
+    res.send("Ola mundo");
+})
+
+
+//server
+app.listen(port, () => {
+    console.log("Servidor rodando na porta "+port);
+})
